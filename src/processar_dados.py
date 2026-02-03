e59e396267d4a55c4b34998e91e33a246dad00fb
isabellageo <isabellageoo@gmail.com>
2026-02-03 17:22:05 -0300

    finalizando 1º teste

--- src/processar_dados.py
+++ src/processar_dados.py
@@ -3,6 +3,7 @@ import zipfile
 import pandas as pd
 
 PASTA_RAW = "data/raw"
+TRIMESTRES = ["2T2024", "3T2024", "4T2024"]
 
 def extrair_zips():
     arquivos = os.listdir(PASTA_RAW)
@@ -29,16 +30,47 @@ def extrair_zips():
 def listar_arquivos_extraidos():
         print("\nArquivos extraídos por trimestre:\n")
 
-        for item in os.listdir(PASTA_RAW):
-            caminho = os.path.join(PASTA_RAW, item)
+        for trimestre in TRIMESTRES:
+            pasta = os.path.join(PASTA_RAW, trimestre)
+            if os.path.exists(pasta):
+                print(trimestre)
 
-            if os.path.isdir(caminho) and not item.endswith(".zip"):
-                print(f"{item}")
+            for arquivo in os.listdir(pasta):
+                print(f" - {arquivo}")
 
-                for arquivo in os.listdir(caminho):
-                    print(f" - {arquivo}")
+def carregar_todos_csvs():
+    dfs = []
 
-                    
+    for trimestre in TRIMESTRES:
+        caminho = os.path.join(PASTA_RAW, trimestre, f"{trimestre}.csv")
+        print(f"\nLendo {caminho}...")
+
+        df = pd.read_csv(caminho, sep=";", encoding="latin1")
+
+        df["VL_SALDO_FINAL"] = (
+            df["VL_SALDO_FINAL"]
+            .astype(str)
+            .str.replace(".", "", regex=False)
+            .str.replace(",", ".", regex=False)
+            .astype(float)
+        )
+
+        df["TRIMESTRE"] = trimestre
+        dfs.append(df)
+
+    df_final= pd.concat(dfs, ignore_index=True)
+    return df_final
+
+   
 if __name__ == "__main__":
     extrair_zips()
     listar_arquivos_extraidos()
+
+    df = carregar_todos_csvs()
+
+    print("\nColunas do DataFrame final:")
+    print (df.columns)
+
+    print("\nTotal de registros:", len(df))
+    print("\nPrimeiras linhas do DataFrame consolidado:")
+    print(df.head())
